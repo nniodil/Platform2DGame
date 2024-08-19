ddc91151f0116800fcc1aec459b6f11eedeff75e
Nadir Niodil <165339957+nniodil@users.noreply.github.com>
2024-08-19 18:23:11 +0200

    Update LaserScript.cs

--- Scripts/LaserScript.cs
+++ Scripts/LaserScript.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 public class LaserScript : MonoBehaviour
 {
     public float speed;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * Time.deltaTime * speed);
